94a3b55ffdf1ae2643d41b584b7b3cb350204925
ZiggiZagga <75372480+ZiggiZagga@users.noreply.github.com>
2025-07-05 13:59:26 +0000

    added ComponentScan packages

--- src/main/java/com/ironbucket/sentinelgear/GatewayApp.java
+++ src/main/java/com/ironbucket/sentinelgear/GatewayApp.java
@@ -4,7 +4,12 @@ package com.ironbucket.sentinelgear;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({			
+			"com.ironbucket.pactumscroll",
+			"com.ironbucket.sentinelgear"
+})
 @SpringBootApplication
 @EnableDiscoveryClient
 class GatewayApp {
