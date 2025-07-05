c8688b4a06b4d277f91ac18a19a247a9b53a9dfa
ZiggiZagga <75372480+ZiggiZagga@users.noreply.github.com>
2025-07-05 13:59:45 +0000

    code cleaning

---
+++ src/main/java/com/ironbucket/sentinelgear/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.ironbucket.sentinelgear;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+}
